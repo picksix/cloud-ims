993fd02429641585d3ec0dc498c70a465cc88b9e
picksix <safazi@bk.ru>
2022-05-01 11:51:44 -0500

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,12 +22,7 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
+@Options({})
 export default class Home extends Vue {}
 </script>
--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -5,8 +5,6 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 
-@Options({
-  props: {},
-})
+@Options({})
 export default class Staff extends Vue {}
 </script>
