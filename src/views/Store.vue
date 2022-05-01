d09b3889e6b674aa13bb433ffcab4c69ee1f059c
Frank Idrogo <f.a.idrogo@gmail.com>
2022-05-01 13:15:24 -0500

    Began product grid

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,5 +1,26 @@
 <template>
 <div>
-  <h1>This is the store page</h1>
+  <h1 class="shop-title">Rowdy Shop</h1>
+  <div class="main-shop">
+    <div class="product-box">
+      <div class="product-item">
+        <img src="" alt="">
+      </div>
+      <div class="product-info">
+      </div>
+      <div class="product-item">
+        <img src="" alt="">
+      </div>
+      <div class="product-info">
+      </div>
+    </div>
+  </div>
 </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Staff extends Vue {}
+</script>
