dce98e2e0df83b485273b393d3036305897a92d0
picksix <safazi@bk.ru>
2022-05-01 11:26:53 -0500

    navbar + transition fade

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Navbar/>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <section class="section" style="padding-top: 20px;">
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </section>
 </template>
 
 <script lang="ts">
@@ -17,3 +19,15 @@ import '@/assets/theme.scss';
 })
 export default class Staff extends Vue {}
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.15s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
   <nav class="navbar is-dark is-bold is-unselectable">
     <div class="navbar-brand">
-      <a class="navbar-burger">
+      <a class="navbar-burger" @click="toggleMenu">
         <span></span>
         <span></span>
         <span></span>
       </a>
     </div>
-    <div class="navbar-menu">
+    <div class="navbar-menu" :class="navbarStyle">
       <div class="navbar-start">
-
+        <router-link to="/" class="navbar-item">Home</router-link>
       </div>
       <div class="navbar-end">
-        <router-link to="staff" class="navbar-item">Staff</router-link>
+        <router-link to="/staff" class="navbar-item">Staff</router-link>
       </div>
     </div>
   </nav>
@@ -22,7 +22,19 @@
 import { Options, Vue } from 'vue-class-component';
 
 @Options({
-  props: {},
+  data: () => ({
+    showMenu: false,
+  }),
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+  },
+  computed: {
+    navbarStyle() {
+      return { 'is-active': this.showMenu };
+    },
+  },
 })
 export default class Navbar extends Vue {
 
