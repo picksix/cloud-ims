055d0fd20245d6983133d603f29ebc52402a37b2
picksix <safazi@bk.ru>
2022-05-01 11:11:35 -0500

    navbar + index

--- src/App.vue
+++ src/App.vue
@@ -17,26 +17,3 @@ import '@/assets/theme.scss';
 })
 export default class Staff extends Vue {}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,21 @@
 <template>
-  <p>test</p>
+  <nav class="navbar is-dark is-bold is-unselectable">
+    <div class="navbar-brand">
+      <a class="navbar-burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+
+      </div>
+      <div class="navbar-end">
+        <router-link to="staff" class="navbar-item">Staff</router-link>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
