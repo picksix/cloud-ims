6b7b6bec8e727863b96ae70d38952eb4a1151ea3
picksix <safazi@bk.ru>
2022-05-03 19:14:30 -0500

    scaffold store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,20 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    products: [],
   },
   mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    },
   },
   actions: {
+    fetchProducts(context) {
+      // TODO: read products from google cloud function
+    },
+    buyProduct(context, payload) {
+      console.log('buying');
+    },
   },
   modules: {
   },
